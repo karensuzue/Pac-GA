332d5cdaa5df306729043e9af46a1bdd3c974ab4
Karen <ksuzue22@wooster.edu>
2022-02-02 21:08:34 -0500

    score system now on UI

--- pacgame/Assets/Scripts/HUDController.cs
+++ pacgame/Assets/Scripts/HUDController.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUDController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public Text scoreText;
+    public GameObject endPopup;
+
+    public int currentScore;
+    public GameObject gameManagerObj;
+    public GameManager gameManager;
+
+    public GameManager.GameStates state;
+
+    void Awake()
     {
-        
+        gameManager = gameManagerObj.GetComponent<GameManager>();
+        currentScore = gameManager.currentScore;
+        scoreText.text = "Score: " + currentScore.ToString();
+        state = gameManager.state;
+        endPopup.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentScore = gameManager.currentScore;
+        scoreText.text = "Score: " + currentScore.ToString();
+        state = gameManager.state;
+
+        if (state == GameManager.GameStates.win)
+        {
+            endPopup.SetActive(true);
+        }
     }
 }
